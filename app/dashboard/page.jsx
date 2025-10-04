6aad9a9785cc8a49ea314f0f52d02d041fa5b249
Clinton Yade <yadeclinton20@gmail.com>
2025-10-04 20:10:31 +0300

    added a dashbord

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+export default function DashboardPage() {
+  return (
+    <div className="text-center py-20">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Dashboard
+      </h1>
+      <p className="text-gray-600">Welcome to your private dashboard!</p>
+    </div>
+  );
+}
